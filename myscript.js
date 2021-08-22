3ac0d5874507a799dea2f7392d98cc6e999e0daf
Shaikh-Mustafa <66483021+Shaikh-Mustafa@users.noreply.github.com>
2021-08-22 13:27:46 +0530

    Add files via upload
    Added Initial Files.

---
+++ myscript.js
@@ -0,0 +1,86 @@
+ // a function to store the text inside the textarea in a variable...
+
+	function analyze(){	
+       var words = 0,letters = 0,spaces = 0,commas = 0;
+ 	   
+	   var text = document.getElementById('text').value;  // this will completely store the text in textarea as a string....	  
+	
+	   //text.toUppercase(); // Name and name will be the same as they are the same words no matter what the case is....
+
+		for(var i = 0 ; i < text.length ; i++){
+		      if(text[i] ==  ' ')
+			    spaces += 1;
+			  else 
+			    letters += 1;
+		 	}
+		
+		for(var i = 0 ; i < text.length ; i++){
+		    if(text[i] == '.' || text[i] == ',')
+			    commas += 1;
+				}
+				
+		words = (spaces + commas) + 1;
+		
+		document.getElementById('spaces').innerHTML = spaces;
+		document.getElementById('letters').innerHTML = letters;
+		document.getElementById('words').innerHTML = words;
+		
+		most_common();
+				
+        }		 
+      		// this code is taken from stackoverflow...
+    function most_common(){
+		var str = document.getElementById('text').value;
+        console.log(findMostRepeatedWord(str)); // Result: "do"
+
+	function findMostRepeatedWord(str) {
+	  let words = str.match(/\w+/g);
+	  console.log(words); // [ 'How', 'do', 'you', 'do' ]
+
+	  let occurances = {};
+
+  for (let word of words) {
+    if (occurances[word]) {
+      occurances[word]++;
+    } else {
+      occurances[word] = 1;
+    }
+  }
+
+  //console.log(occurances); // { How: 1, do: 2, you: 1 }
+
+  let max = 0;
+  let mostRepeatedWord = '';
+
+  for (let word of words) {
+    if (occurances[word] > max) {
+      max = occurances[word];
+      mostRepeatedWord = word;
+    }
+  }
+  document.getElementById('occurance').innerHTML = max;
+  document.getElementById('common').innerHTML = mostRepeatedWord;
+}
+	}
+	
+	function changemode(val){
+	
+	 if(val == 1){
+		document.getElementById('body').style.backgroundColor = "#3B3C36";   //color name = oynx..
+		
+		
+	}
+	 else{
+	    document.getElementById('body').style.backgroundColor = "#E7FEFF";
+		
+	}
+}
+
+function showtime()
+{   
+	document.getElementById('time').innerHTML = Date();
+}
+
+var Interval = setInterval(showtime,1000);
+
+// Sucessfully done... by me yeah......
